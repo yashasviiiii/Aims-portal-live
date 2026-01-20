2d9e7b5781ba3731480fea016a06326a42aff678
krishn113 <ag.krishna0807@gmail.com>
2026-01-21 01:09:02 +0530

    fixes login api

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/verify-otp", verifyOtp);
-router.post("/", login);
+router.post("/login", login);
 
 export default router;
 
